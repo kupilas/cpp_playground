f5ad4903753864648cc8265ebbdf71a9fb0f86b8
Matt <mkupilas@gmail.com>
2015-07-10 17:05:18 +0200

    added sprite with gravity

--- CocosLearningProject/CocosLearningProject/Classes/PhysicsSene.cpp
+++ CocosLearningProject/CocosLearningProject/Classes/PhysicsSene.cpp
@@ -12,6 +12,7 @@ Scene* PhysicsSene::createScene()
 {
     auto scene = Scene::createWithPhysics();
     scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+    scene->getPhysicsWorld()->setGravity(Vec2(0, -200));
     
     auto layer = PhysicsSene::create();
     layer->setPhysicsWorld(scene->getPhysicsWorld());
@@ -34,10 +35,18 @@ bool PhysicsSene::init()
     auto edgeBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
     
     auto edgeNode = Node::create();
-    edgeNode ->setPosition( Point( visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y ) );
-    edgeNode->setPhysicsBody( edgeBody );
+    edgeNode ->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    edgeNode->setPhysicsBody(edgeBody);
     
-    this->addChild( edgeNode );
+    this->addChild(edgeNode);
+    
+    auto sprite = Sprite::create( "CloseNormal.png" );
+    sprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    
+    auto spriteBody = PhysicsBody::createCircle(sprite->getContentSize().width / 2, PhysicsMaterial(0, 1, 0));
+    sprite->setPhysicsBody(spriteBody);
+    
+    this->addChild(sprite);
     
     CCLOG("PhysicsSene::init()");
     
