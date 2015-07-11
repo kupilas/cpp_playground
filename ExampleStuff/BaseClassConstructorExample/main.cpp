3712aa84f0349fcad3fbb20fd5c32888416d4d09
Matt <mkupilas@gmail.com>
2015-07-11 23:02:32 +0200

    Accessing Overridden Methods example

--- ExampleStuff/BaseClassConstructorExample/main.cpp
+++ ExampleStuff/BaseClassConstructorExample/main.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
+class SomeAbstractClass{
+	virtual void someFunction() = 0;
+};
+
 
 class Person{
 
 	public:
 	 Person(int a){
 		cout << "Person contructor with: " << a << endl;
+	}
+
+	void methodA(int a)
+	{
+		cout << "methodA with: " << a << endl;
 	}
 
 };
@@ -20,11 +29,19 @@ class Dude : public Person{
 			cout << "Dude constructor with: " << b << endl;
 		}
 
+		void methodA(int a)
+		{
+			Person::methodA(a);
+			cout << "overriden methodA with: " << a << endl;
+		}
+
 };
 
 int main(){
 
 	Dude maniek(1);
+	maniek.methodA(5);
+	maniek.Person::methodA(5);
 
 	return 0;
 }
